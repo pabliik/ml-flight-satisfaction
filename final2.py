4e4925c060b6a8ed07b8a5990363a77b615d53f9
Pavlo Asieiev <726030@student.inholland.nl>
2026-06-07 23:04:19 +0200

    stratify

--- final2.py
+++ final2.py
@@ -30,7 +30,7 @@ df = df.drop(columns=['Departure Delay'])
 # 3. Split
 X = df.drop(columns='Satisfaction')
 y = df['Satisfaction']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42,stratify=y)
 
 # Verify split
 print(f"Training set shape: {X_train.shape}")
